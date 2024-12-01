75e54ec381b93a6385959b8a71b59b4ce11fbfcb
Kichirou <mrphuc244@gmail.com>
2024-12-02 00:25:22 +0700

    [ADD] - add step 2 two pointers

---
+++ Two Pointers Method/Step 2/A.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define taskname "A"
+
+int nTest = 1;
+
+#define pb(x, y) x.push_back(y);
+#define fi first
+#define se second
+#define int long long
+#define c1(x) cout << x << " ";
+#define c2(x, y) cout << x << " " << y << " ";
+#define ce1(x) cout << x << endl;
+#define ce2(x, y) cout << x << " " << y << endl;
+
+const int MAXN = 2e6 + 10;
+const int MOD = 1e9 + 7;
+
+typedef pair<int, int> PII;
+
+void Input()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    if (fopen(taskname ".inp", "r"))
+    {
+        freopen(taskname ".inp", "r", stdin);
+        freopen(taskname ".out", "w", stdout);
+    }
+    // cin >> nTest;
+}
+
+void Solve()
+{
+    int n, s;
+    cin >> n >> s;
+
+    vector<int> a(n);
+    for (auto &x : a)
+        cin >> x;
+
+    int i = 0, res = 0, sum = 0;
+    for (int j = 0; j < n; ++j)
+    {
+        sum += a[j];
+        while (sum > s)
+            sum -= a[i++];
+
+        res = max(res, j - i + 1);
+    }
+
+    ce1(res)
+}
+
+signed main()
+{
+    clock_t begin = clock();
+    Input();
+    while (nTest--)
+        Solve();
+
+cerr << "Time: " << (clock() - begin + 1.0) / CLOCKS_PER_SEC << "s\n";
+    if (((clock() - begin + 1.0) / CLOCKS_PER_SEC) > 2.0)
+        cerr << "TLE\n";
+    return 0;
+}
